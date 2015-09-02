5d895081d193983057a8196dbc5aacbc1a0fc526
dean <deang@hotmail.com.au>
2015-09-02 22:29:11 +1000

    act next

--- app/src/main/java/au/com/deangoddard_17822594/assignment_1/MainActivity.java
+++ app/src/main/java/au/com/deangoddard_17822594/assignment_1/MainActivity.java
@@ -3,6 +3,7 @@ package au.com.deangoddard_17822594.assignment_1;
 
 //master cameronsone
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +17,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        Button friends = (Button)findViewById(R.id.Freinds);
+
+       friends.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(MainActivity.this, Freinds.class));
+           }
+       });
+
+        Button cal = (Button)findViewById(R.id.Calender);
+
+       cal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Event.class));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
