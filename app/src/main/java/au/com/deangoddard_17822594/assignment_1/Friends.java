7bd2ead6f38196445c211726ae2f76b66589aa0a
wikit <deang@hotmail.com.au>
2015-09-06 00:19:37 +1000

    PO-02with sql

--- app/src/main/java/au/com/deangoddard_17822594/assignment_1/Friends.java
+++ app/src/main/java/au/com/deangoddard_17822594/assignment_1/Friends.java
@@ -1,37 +1,152 @@
 package au.com.deangoddard_17822594.assignment_1;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Friends extends AppCompatActivity {
 
+import android.widget.EditText;
+
+public class Friends extends Activity implements OnClickListener
+{
+    EditText editRollno,editName,editMarks;
+    Button btnAdd,btnDelete,btnModify,btnView,btnViewAll;
+    SQLiteDatabase db;
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friends);
+        setContentView(R.layout.main);
+        editRollno=(EditText)findViewById(R.id.editRollno);
+        editName=(EditText)findViewById(R.id.editName);
+        editMarks=(EditText)findViewById(R.id.editMarks);
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+        btnDelete=(Button)findViewById(R.id.btnDelete);
+        btnModify=(Button)findViewById(R.id.btnModify);
+        btnView=(Button)findViewById(R.id.btnView);
+        btnViewAll=(Button)findViewById(R.id.btnViewAll);
+
+        btnAdd.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnModify.setOnClickListener(this);
+        btnView.setOnClickListener(this);
+        btnViewAll.setOnClickListener(this);
+
+        db=openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_freinds, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view)
+    {
+        if(view==btnAdd)
+        {
+            if(editRollno.getText().toString().length()==0||
+                    editName.getText().toString().length()==0||
+                    editMarks.getText().toString().length()==0)
+            {
+                showMessage("Error", "Please enter all values");
+                return;
+            }
+            db.execSQL("INSERT INTO student VALUES('"+editRollno.getText()+"','"+editName.getText()+
+                    "','"+editMarks.getText()+"');");
+            showMessage("Success", "Record added");
+            clearText();
+        }
+        if(view==btnDelete)
+        {
+            if(editRollno.getText().toString().trim().length()==0)
+            {
+                showMessage("Error", "Please enter Rollno");
+                return;
+            }
+            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+            if(c.moveToFirst())
+            {
+                db.execSQL("DELETE FROM student WHERE rollno='"+editRollno.getText()+"'");
+                showMessage("Success", "Record Deleted");
+            }
+            else
+            {
+                showMessage("Error", "Invalid Rollno");
+            }
+            clearText();
+        }
+        if(view==btnModify)
+        {
+            if(editRollno.getText().toString().trim().length()==0)
+            {
+                showMessage("Error", "Please enter Rollno");
+                return;
+            }
+            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+            if(c.moveToFirst())
+            {
+                db.execSQL("UPDATE student SET name='"+editName.getText()+"',marks='"+editMarks.getText()+
+                        "' WHERE rollno='"+editRollno.getText()+"'");
+                showMessage("Success", "Record Modified");
+            }
+            else
+            {
+                showMessage("Error", "Invalid Rollno");
+            }
+            clearText();
+        }
+        if(view==btnView)
+        {
+            if(editRollno.getText().toString().trim().length()==0)
+            {
+                showMessage("Error", "Please enter Rollno");
+                return;
+            }
+            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+            if(c.moveToFirst())
+            {
+                editName.setText(c.getString(1));
+                editMarks.setText(c.getString(2));
+            }
+            else
+            {
+                showMessage("Error", "Invalid Rollno");
+                clearText();
+            }
+        }
+        if(view==btnViewAll)
+        {
+            Cursor c=db.rawQuery("SELECT * FROM student", null);
+            if(c.getCount()==0)
+            {
+                showMessage("Error", "No records found");
+                return;
+            }
+            StringBuffer buffer=new StringBuffer();
+            while(c.moveToNext())
+            {
+                buffer.append("Rollno: "+c.getString(0)+"\n");
+                buffer.append("Name: "+c.getString(1)+"\n");
+                buffer.append("Marks: "+c.getString(2)+"\n\n");
+            }
+            showMessage("Student Details", buffer.toString());
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+    public void showMessage(String title,String message)
+    {
+        Builder builder=new Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+    public void clearText()
+    {
+        editRollno.setText("");
+        editName.setText("");
+        editMarks.setText("");
+        editRollno.requestFocus();
     }
 }
